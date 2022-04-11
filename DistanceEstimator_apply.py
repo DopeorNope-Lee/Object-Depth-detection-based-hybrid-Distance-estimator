4f620dadf2b33f310e9898a381e97b1bee9b40f3
dopeornope-Lee <luj0407@gmail.com>
2022-04-11 18:41:11 +0900

    no message

--- DistanceEstimator_apply.py
+++ DistanceEstimator_apply.py
@@ -1,3 +1,8 @@
+"""
+    @ Author :ODD_team
+"""
+
+from sklearn import linear_model
 from tqdm import tqdm
 import pandas as pd
 import numpy as np
@@ -7,6 +12,23 @@ from torch import nn
 from torch.utils.data import TensorDataset
 from torch.utils.data import DataLoader 
 from sklearn.preprocessing import StandardScaler
+from torchvision import transforms
+from transformers import GLPNForDepthEstimation, GLPNFeatureExtractor
+from transformers import DetrFeatureExtractor, DetrForObjectDetection
+from transformers import GLPNForDepthEstimation, GLPNFeatureExtractor
+from PIL import Image
+import cv2
 
 
 
+
+
+
+"""
+
+# 모델 불러오기
+DE_PATH="aaaa"
+model = linear_model()
+model.load_state_dict(torch.load('DE_PATH'))
+model.eval()
+"""
